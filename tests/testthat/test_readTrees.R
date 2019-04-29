ac263310ddee27630a14fc97da8265936f225581
Michael R May <mikeryanmay@gmail.com>
2019-04-29 12:22:46 -0700

    disabling vignettes for travis

--- tests/testthat/test_readTrees.R
+++ tests/testthat/test_readTrees.R
@@ -1,21 +1,22 @@
 context("tests the readTrees function")
 
-test_that("dummy test", {
-  t <- 5
-  expect_equal(5, t)
+# test_that("dummy test", {
+#   t <- 5
+#   expect_equal(5, t)
+# })
+
+test_that("reads single tree", {
+  file <- system.file("extdata", "sub_models/primates_cytb_covariotide_MAP.tre", package="RevGadgets")
+  tree_single <- readTrees(path = file, format = "nexus")
+  expect_equal(length(tree_single), 1)
+  expect_equal(class(tree_single), "multiPhylo")
+  expect_equal(length(tree_single[[1]]$tip.label), 23)
 })
 
-
-# test_that("reads single tree", {
-#   tree_single <- readTrees(path = "../../data/sub_models/primates_cytb_covariotide_MAP.tre", format = "nexus")
-#   expect_equal(length(tree_single), 1)
-#   expect_equal(class(tree_single), "multiPhylo")
-#   expect_equal(length(tree_single[[1]]$tip.label), 23)
-# })
-#
-# test_that("reads multi tree", {
-#   tree_multi <- readTrees(path = "../../data/sub_models/primates_cytb_covariotide.trees", format = "newick")
-#   expect_equal(length(tree_multi), 4002)
-#   expect_equal(class(tree_multi), "multiPhylo")
-#   expect_equal(length(tree_multi[[1]]$tip.label), 23)
-# })
+test_that("reads multi tree", {
+  file <- system.file("extdata", "sub_models/primates_cytb_covariotide.trees", package="RevGadgets")
+  tree_multi <- readTrees(path = file, format = "newick")
+  expect_equal(length(tree_multi), 4002)
+  expect_equal(class(tree_multi), "multiPhylo")
+  expect_equal(length(tree_multi[[1]]$tip.label), 23)
+})
