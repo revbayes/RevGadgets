6950de3ea235a037679da45fea4db830324b6d0b
cmt2 <cmt2@berkeley.edu>
2019-08-18 14:46:16 -0700

    updating readTrees() to grad taxon names from translate block in nexus files.

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,43 @@
 # Non-exported utility functions for RevGadgets
+buildTranslateDictionary <- function(lines) {
+
+  start_tree_block <- grep("begin trees;", lines, ignore.case = TRUE)
+  end_tree_block <- grep("end;", lines[start_tree_block:length(lines)], ignore.case = TRUE)[1] + start_tree_block - 1
+  tree_block <- lines[start_tree_block : end_tree_block]
+
+  # look for translate block
+
+  start_translations <- grep("translate", tree_block, ignore.case = TRUE)
+  end_translations <- grep(";", tree_block[start_translations:length(tree_block)])[1] + start_translations -  1
+  translations <- tree_block[start_translations : end_translations]
+  translations <- gsub("\\\t","", translations)
+  translations <- gsub(",", "", translations)
+
+  # grab only the numbers and taxa names
+  translations <- translations[grep("[1-9]", translations)]
+  translations_split <- lapply(translations, strsplit, split = " ")
+  dictionary <- as.data.frame(matrix(unlist(translations_split),
+                                     ncol = 2, byrow = TRUE),
+                              stringsAsFactors = FALSE)
+  colnames(dictionary) <- c("number","taxon")
+
+  return(dictionary)
+}
+
+findTreeLines <- function(lines) {
+
+  # pull out tree block only
+  start_tree_block <- grep("begin trees;", lines, ignore.case = TRUE)
+  end_tree_block <- grep("end;", lines[start_tree_block:length(lines)], ignore.case = TRUE)[1] + start_tree_block - 1
+  tree_block <- lines[start_tree_block : end_tree_block]
+
+  # pull out trees
+  tree_strings <- tree_block[grep("tree ", tree_block, ignore.case=TRUE)]
+
+  # return tree strings
+  return(tree_strings)
+
+}
 
 isNexusFile <- function(file) readLines(file, n=1) == "#NEXUS"
 
@@ -16,7 +55,7 @@ readNexusTrees <- function(path, burnin, verbose, ...) {
   lines <- readLines(path)
 
   # the line with a tree
-  tree_strings <- lines[grep("tree ", lines, ignore.case=FALSE)]
+  tree_strings <- findTreeLines(lines)
 
   # discard burnin (if provided)
   if (burnin >= 1) {
@@ -42,6 +81,18 @@ readNexusTrees <- function(path, burnin, verbose, ...) {
   }
   if ( verbose == TRUE ) {
     cat("\n")
+  }
+
+  # translate using dictionary if translate block present in file
+  if (length(grep("translate", lines, ignore.case = TRUE)) >= 1) {
+    dictionary <- buildTranslateDictionary(lines = lines)
+    for (i in 1:n_trees) {
+      n_tips <- length(trees[[i]]@phylo$tip.label)
+      for (j in 1:n_tips) {
+        ind <- which(trees[[i]]@phylo$tip.label[j] == dictionary[, 1])
+        trees[[i]]@phylo$tip.label[j] <- dictionary[ind, 2]
+      }
+    }
   }
 
   # return the trees
