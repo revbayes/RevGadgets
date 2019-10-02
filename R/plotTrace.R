cb68e1d0694495925c8161aa7aa2330a3ba20969
cmt2 <cmt2@berkeley.edu>
2019-10-01 19:21:04 -0700

    updating colors

--- R/plotTrace.R
+++ R/plotTrace.R
@@ -17,15 +17,10 @@
 #' contains multiple traces (such as from multiple runs), summarizeTrace() will provide
 #' summaries for each trace individually, as well as the combined trace.
 #'
-#' @param vars (character or character vector; no default) The name of the variable(s)
+#' @param vars (character or character vector; NULL) The specific name(s) of the variable(s)
 #' to be summarized.
 #'
-#' @param plot_together (logical; TRUE) Plot all variables together?
-#'
-#' @param colors (character vector; "Set1") Vector of colors for plotting, containing
-#' the name of a Brewer palette, hex codes, or R-approved color names. Default behavior
-#' is to plot using Color Brewer's Set1 palette.
-#'
+#' @param match (character; NULL) Plot all variables together?
 #'
 #' @return summarizeTrace() returns a list of the length of provided variables. For quantitative
 #' variables, it returns the mean and 95% credible interval. For discrete variables, it returns
@@ -38,10 +33,8 @@
 #' file <- system.file("extdata",
 #'     "sub_models/primates_cytb_covariotide.p", package="RevGadgets")
 #' one_trace <- readTrace(path = file)
-#' trace_sum <- summarizeTrace(trace = one_trace,
-#'                             vars = c("Likelihood",
-#'                                      "lambda",
-#'                                    "TL"))
+#' plots <- plotTrace(trace = one_trace,
+#'                     vars = c("pi[1]","pi[2]","pi[3]","pi[4]"))
 #'
 #' # discrete character example, multiple runs
 #' file1 <- system.file("extdata",
@@ -62,14 +55,15 @@
 #' @export
 
 
-plotTrace <- function(trace, vars, colors = "Set1", plot_together = TRUE) {
+plotTrace <- function(trace, vars = NULL, match = NULL) {
 
   # enforce argument matching
   if (is.list(trace) == FALSE) stop("trace should be a list of data frames")
   if (is.data.frame(trace[[1]]) == FALSE) stop("trace should be a list of data frames")
-  if (is.character(vars) == FALSE) stop("vars should be a character vector")
-  if (is.character(colors) == FALSE) stop("colors should be a character vector or single string")
-  if (is.logical(plot_together) == FALSE) stop("combine should be either TRUE or FALSE")
+  if (is.null(vars) & is.null(match)) stop("Either vars or match should be specified")
+  if (!is.null(vars) & !is.null(match)) stop("Only vars OR match should be specified")
+  if (is.character(vars) == FALSE | !is.null(vars)) stop("vars should be a character vector")
+  if (is.character(match) == FALSE | !is.null(match)) stop("vars should be a character vector")
 
   # ensure variable names present in data frame
   if (any(vars %in% colnames(trace[[1]]) == FALSE) == TRUE) {
@@ -78,49 +72,52 @@ plotTrace <- function(trace, vars, colors = "Set1", plot_together = TRUE) {
     stop("oops!")
   }
 
-  if (colors == "Set1"){
-    if (plot_together == TRUE) {
-      plots <- list()
-      for(i in 1:length(trace)){
-        if (length(vars) > 1) {
-          trace[[i]][,vars] %>%
-            melt() %>%
-            ggplot(aes(x = value, fill = variable)) +
-            geom_density(alpha = 0.3) +
-            scale_fill_brewer(palette = "Set1") +
-            theme_few() +
-            ggtitle(label = paste("Trace",i, sep = " ") ) ->
-            plots[[i]]
-        } else if (length(vars) == 1) {
-          data.frame(value = trace[[i]][,vars]) %>%
-            ggplot(aes(x = value)) +
-            geom_density(fill = "#e41a1c") +
-            theme_few() +
-            ggtitle(label = paste0("Trace ",i,": ",vars)) ->
-            plots[[i]]
-        }
+    plots <- list()
+    for(i in 1:length(trace)){
+      if (length(vars) > 1) {
+        trace[[i]][,vars] %>%
+          melt() %>%
+          ggplot(aes(x = value, fill = variable)) +
+          geom_density(alpha = 0.5) +
+          scale_fill_manual(values = colFun(length(vars))) +
+          #scale_fill_brewer(palette = "Set1") +
+          theme_few() +
+          ggtitle(label = paste("Trace",i, sep = " ") ) +
+          theme(plot.title = element_text(hjust = 0.5)) ->
+          plots[[i]]
+      } else if (length(vars) == 1) {
+        data.frame(value = trace[[i]][,vars]) %>%
+          ggplot(aes(x = value)) +
+          geom_density(fill = "#e41a1c") +
+          theme_few() +
+          ggtitle(label = paste0("Trace ",i,": ",vars)) +
+          theme(plot.title = element_text(hjust = 0.5)) ->
+          plots[[i]]
       }
-      # add combined trace plots if multiple traces provided
-      if (length(trace) > 1){
-        if (length(vars) > 1) {
-          do.call("rbind", trace)[,vars] %>%
-            melt() %>%
-            ggplot(aes(x = value, fill = variable)) +
-            geom_density(alpha = 0.3) +
-            scale_fill_brewer(palette = "Set1") +
-            theme_few() +
-            ggtitle(label = "Combined Trace:") ->
-            plots[[length(trace) + 1]]
-        } else if (length(vars) == 1) {
-          data.frame(value =  do.call("rbind", trace)[,vars]) %>%
-            ggplot(aes(x = value)) +
-            geom_density(fill = "#e41a1c") +
-            theme_few() +
-            ggtitle(label = paste("Combined Trace:", vars)) ->
-            plots[[length(trace) + 1]]
-        }
+    }
+    # add combined trace plots if multiple traces provided
+    if (length(trace) > 1){
+      if (length(vars) > 1) {
+        do.call("rbind", trace)[,vars] %>%
+          melt() %>%
+          ggplot(aes(x = value, fill = variable)) +
+          geom_density(alpha = 0.5) +
+          #scale_fill_brewer(palette = "Set1") +
+          scale_fill_manual(values = colFun(length(vars))) +
+          theme_few() +
+          ggtitle(label = "Combined Trace:") +
+          theme(plot.title = element_text(hjust = 0.5)) ->
+          plots[[length(trace) + 1]]
+      } else if (length(vars) == 1) {
+        data.frame(value =  do.call("rbind", trace)[,vars]) %>%
+          ggplot(aes(x = value)) +
+          geom_density(fill = "#e41a1c") +
+          theme_few() +
+          ggtitle(label = paste("Combined Trace:", vars)) +
+          theme(plot.title = element_text(hjust = 0.5)) ->
+          plots[[length(trace) + 1]]
       }
-    } else {stop("haven't yet developed functionality for plot_together = FALSE")}
-  } else {stop("haven't yet developed functionality for other colors")}
+    }
+
   return(plots)
 }
