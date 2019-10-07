592448c1c066d0ac02f0a4e596f22b105bdeeba2
Carrie <carrie@mrmay.local>
2019-10-07 07:17:29 -0700

    adding explicit calls to ggplot2 in plotTrace()

--- R/plotTrace.R
+++ R/plotTrace.R
@@ -20,35 +20,32 @@
 #' @param vars (character or character vector; NULL) The specific name(s) of the variable(s)
 #' to be summarized.
 #'
-#' @param match (character; NULL) Plot all variables together?
+#' @param match (character; NULL) A string to match to a group of parameters. For example,
+#' match = "er" will plot the variables "er[1]", "er[2]", "er[3]", etc.. match
+#' will only work if your search string is followed by brackets in one or more of
+#'  the column names of the provided trace file. match = "er" will only return the
+#'  exchangeability parameters, but will not plot "Posterior".
 #'
-#' @return summarizeTrace() returns a list of the length of provided variables. For quantitative
-#' variables, it returns the mean and 95% credible interval. For discrete variables, it returns
-#' the 95% credible set of states and their associated probabilities.
+#' @return plotTrace() returns a list of the length of provided trace object, plus one
+#' combined trace. Each element of the list contains a ggplot object with plots of
+#' the provided parameters. These plots may be modified in typical ggplot fashion.
 #'
 #' @examples
 #'
 #' \dontrun{
-#' # continuous character only example, one run
+#'
 #' file <- system.file("extdata",
 #'     "sub_models/primates_cytb_covariotide.p", package="RevGadgets")
 #' one_trace <- readTrace(path = file)
 #' plots <- plotTrace(trace = one_trace,
 #'                     vars = c("pi[1]","pi[2]","pi[3]","pi[4]"))
+#' plots[[1]]
+#' # add custom colors
+#' plots[[1]] + scale_fill_manual(values = c("green","red","blue","orange"))
 #'
-#' # discrete character example, multiple runs
-#' file1 <- system.file("extdata",
-#'     "comp_method_disc/mkstates_run_1.txt", package="RevGadgets")
-#' file2 <- system.file("extdata",
-#'     "comp_method_disc/mkstates_run_2.txt", package="RevGadgets")
+#' # make the same plot, using match
 #'
-#' multi_trace <- readTrace(path = c(file1, file2))
-#' trace_sum_multi <- summarizeTrace(trace = multi_trace,
-#'                                   vars = c("end_680",
-#'                                            "end_681",
-#'                                            "end_682",
-#'                                            "end_683"))
-
+#' plots <- plotTrace(trace = one_trace, match = "pi")
 #'
 #' }
 #'
@@ -62,60 +59,62 @@ plotTrace <- function(trace, vars = NULL, match = NULL) {
   if (is.data.frame(trace[[1]]) == FALSE) stop("trace should be a list of data frames")
   if (is.null(vars) & is.null(match)) stop("Either vars or match should be specified")
   if (!is.null(vars) & !is.null(match)) stop("Only vars OR match should be specified")
-  if (is.character(vars) == FALSE | !is.null(vars)) stop("vars should be a character vector")
-  if (is.character(match) == FALSE | !is.null(match)) stop("vars should be a character vector")
+  if (is.character(vars) == FALSE & !is.null(vars)) stop("vars should be a character vector")
+  if (is.character(match) == FALSE & !is.null(match)) stop("match should be a character vector")
 
   # ensure variable names present in data frame
-  if (any(vars %in% colnames(trace[[1]]) == FALSE) == TRUE) {
-    cat("The following variables you provided are not present in trace file:",
-        paste0("\t", vars[!vars %in% colnames(trace[[1]])]), sep = "\n")
-    stop("oops!")
+  if (!is.null(vars)){
+    if (any(vars %in% colnames(trace[[1]]) == FALSE) == TRUE) {
+      cat("The following variables you provided are not present in trace file:",
+          paste0("\t", vars[!vars %in% colnames(trace[[1]])]), sep = "\n")
+      stop("oops!")
+    }
+  }
+  # find matching column names if using match
+  if (!is.null(match)) {
+    vars <- colnames(trace[[1]])[grep(paste0("(",match,")(\\[)"),colnames(trace[[1]]))]
+    if (length(vars) == 0) {stop("match did not correspond to any column names in provided trace")}
   }
 
     plots <- list()
     for(i in 1:length(trace)){
       if (length(vars) > 1) {
-        trace[[i]][,vars] %>%
-          melt() %>%
-          ggplot(aes(x = value, fill = variable)) +
-          geom_density(alpha = 0.5) +
-          scale_fill_manual(values = colFun(length(vars))) +
-          #scale_fill_brewer(palette = "Set1") +
-          theme_few() +
-          ggtitle(label = paste("Trace",i, sep = " ") ) +
-          theme(plot.title = element_text(hjust = 0.5)) ->
-          plots[[i]]
+        t <- trace[[i]][,vars]
+        t <- reshape::melt(t)
+        plots[[i]] <- ggplot2::ggplot(data = t, ggplot2::aes(x = value, fill = variable)) +
+                      ggplot2::geom_density(alpha = 0.5) +
+                      ggplot2::scale_fill_manual(values = colFun(length(vars))) +
+                      ggthemes::theme_few() +
+                      ggplot2::ggtitle(label = paste("Trace",i, sep = " ") ) +
+                      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
       } else if (length(vars) == 1) {
-        data.frame(value = trace[[i]][,vars]) %>%
-          ggplot(aes(x = value)) +
-          geom_density(fill = "#e41a1c") +
-          theme_few() +
-          ggtitle(label = paste0("Trace ",i,": ",vars)) +
-          theme(plot.title = element_text(hjust = 0.5)) ->
-          plots[[i]]
+        t <- data.frame(value = trace[[i]][,vars])
+        plots[[i]] <- ggplot2::ggplot(data = t, ggplot2::aes(x = value)) +
+          ggplot2::geom_density(fill = "#e41a1c") +
+          ggthemes::theme_few() +
+          ggplot2::ggtitle(label = paste0("Trace ",i,": ",vars)) +
+          ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
       }
     }
     # add combined trace plots if multiple traces provided
     if (length(trace) > 1){
       if (length(vars) > 1) {
-        do.call("rbind", trace)[,vars] %>%
-          melt() %>%
-          ggplot(aes(x = value, fill = variable)) +
-          geom_density(alpha = 0.5) +
-          #scale_fill_brewer(palette = "Set1") +
-          scale_fill_manual(values = colFun(length(vars))) +
-          theme_few() +
-          ggtitle(label = "Combined Trace:") +
-          theme(plot.title = element_text(hjust = 0.5)) ->
-          plots[[length(trace) + 1]]
+        t <- do.call("rbind", trace)[,vars]
+        t <- reshape::melt(t)
+        plots[[length(trace) + 1]] <- ggplot2::ggplot(data = t, ggplot2::aes(x = value, fill = variable)) +
+          ggplot2::geom_density(alpha = 0.5) +
+          ggplot2::scale_fill_manual(values = colFun(length(vars))) +
+          ggthemes::theme_few() +
+          ggplot2::ggtitle(label = "Combined Trace:") +
+          ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
       } else if (length(vars) == 1) {
-        data.frame(value =  do.call("rbind", trace)[,vars]) %>%
-          ggplot(aes(x = value)) +
-          geom_density(fill = "#e41a1c") +
-          theme_few() +
-          ggtitle(label = paste("Combined Trace:", vars)) +
-          theme(plot.title = element_text(hjust = 0.5)) ->
-          plots[[length(trace) + 1]]
+        t <- data.frame(value =  do.call("rbind", trace)[,vars])
+        plots[[length(trace) + 1]] <- ggplot2::ggplot(data = t, ggplot2::aes(x = value)) +
+          ggplot2::geom_density(fill = "#e41a1c") +
+          ggthemes::theme_few() +
+          ggplot2::ggtitle(label = paste("Combined Trace:", vars)) +
+          ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
+
       }
     }
 
