0f24363b7485842508fc4a304a2e8166270e1d95
cmt2 <cmt2@berkeley.edu>
2019-11-25 09:00:21 -0800

    adding working plot testing script

--- tests/testthat/test_plotTrace.R
+++ tests/testthat/test_plotTrace.R
@@ -4,11 +4,12 @@ test_that("reads one trace", {
   file_1 <- system.file("extdata",
                       "sub_models/primates_cytb_covariotide.p",
                       package="RevGadgets")
-  one_trace <- readTrace(path = file_1, burnin = 0)
+  one_trace <- readTrace(path = file_1)
   plots_new <- plotTrace(trace = one_trace,
                      vars = c("pi[1]","pi[2]","pi[3]","pi[4]"))
+  # load the saved plot for comparison
   file_2 <- system.file("extdata",
-                        "graphs/plotTrace_pi.RData",
+                        "graphs/plotTrace_pi.Rdata",
                         package="RevGadgets")
   load(file_2)
   expect_equal(plots[[1]], plots_new[[1]])
