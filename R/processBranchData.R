6fc3718f2285bf0e3bb0f6bb5e2d9c9976cec5d6
cmt2 <cmt2@berkeley.edu>
2021-02-05 18:20:56 -1000

    updating processBranchData() documentation to use RevGadgets processing scripts

--- R/processBranchData.R
+++ R/processBranchData.R
@@ -11,16 +11,12 @@
 #'
 #' @examples
 #' \dontrun{
-#' library(RevGadgets)
-#' library(tidytree)
-#' library(ggplot2)
-#' library(ape)
 #'
 #' treefile <- system.file("extdata", "bds/primates.tre", package="RevGadgets")
 #' logfile <- system.file("extdata", "bds/primates_BDS_rates_truncated.p", package="RevGadgets")
 #'
-#' branch_data <- read.table(logfile, header = TRUE, sep = "\t")
-#' tree <- tidytree::as.treedata(read.tree(treefile))
+#' branch_data <- readTrace(logfile)[[1]]
+#' tree <- readTrees(paths = treefile)[[1]][[1]]
 #'
 #' annotated_tree <- processBranchData(tree, branch_data, summary = "median")
 #'
@@ -29,20 +25,31 @@
 #'   theme(legend.position=c(0.2,0.80), legend.background=element_blank()) +
 #'   scale_color_continuous("Posterior median \nspeciation rate",
 #'                          low="blue", high="green")
+#'
+#' # OR
+#'
+#' p <- plotTree(tree = list(list(annotated_tree)),
+#'               node_age_bars = FALSE,
+#'               node_pp = F,
+#'               tip_labels = FALSE,
+#'               color_branch_by = "avg_lambda",
+#'               line_width = 0.8) +
+#'      ggplot2::theme(legend.position=c(.1, .9))
+#'
 #' }
 processBranchData <- function(tree, df, burnin = 0.25,
                               parnames = c("avg_lambda", "avg_mu", "num_shifts"),
                               summary = "median"){
 
   df <- df[floor(nrow(df)*burnin):nrow(df),]
-  tree_tbl <- as_tibble(tree)
+  tree_tbl <- tibble::as_tibble(tree)
   map <- matchNodes(tree@phylo)
 
   for (item in parnames){
     parameter <- unname(sapply(df[,grepl(item, colnames(df))], summary))[map$Rev]
     tree_tbl[[item]] <- parameter
   }
-  tree2 <- as.treedata(tree_tbl)
+  tree2 <- tidytree::as.treedata(tree_tbl)
 
   return(tree2)
 }
