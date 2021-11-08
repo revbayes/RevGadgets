a10388d772f593c4dc7420ea7841a835fee2bb89
cmt2 <cmt2@berkeley.edu>
2021-11-08 09:21:43 -0800

    removing good files from legacy branch

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(RevGadgets)
-
-test_check("RevGadgets")
--- tests/testthat/test_colFun.R
+++
@@ -1,5 +0,0 @@
-context("Tests colFun()")
-
-test_that("colFun() doesn't error out", {
-  expect_error(colFun(1), NA)
-})
