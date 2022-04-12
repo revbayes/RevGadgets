83e03f025df3fd9719572783fd0a5e1d7576c200
rbillenstein <ronja@billenstein.com>
2022-04-12 16:37:33 +0200

    minor changes, mainly extdata files and documentation

--- R/plotPopSizes.R
+++ R/plotPopSizes.R
@@ -4,16 +4,13 @@
 #'
 #' Plots the output of coalescent demographic analyses. Takes as
 #' input the output of processPopSizes() and plotting parameters.
-#' For now, only variable names that contain the word "size" are
-#' included in the plot.
 #'
 #' The return object can be manipulated. For example, you can change the
 #' axis labels, the color palette, whether the axes are to be linked, or the
 #' overall plotting style/theme, just as with any ggplot object.
 #'
 #' @param df (data frame) such as produced by processPopSizes(), containing 
-#' the data on population sizes and, if applicable, interval times
-#' @param plot_var (string, default: "size") only include variables that contain the string, default "size" in the name
+#' the data on population sizes and corresponding grid points (points in time for population size evaluation)
 #' @param add (boolean; default: TRUE) specifies whether the new plot should be added to an existing ggplot2 object. If TRUE,
 #' the existing_plot has to be given.
 #' @param existing_plot (ggplot2 object; default: NULL) a ggplot2 object to which the new plot should be added.
@@ -26,11 +23,9 @@
 #' library(tibble)
 #' 
 #' df <- tibble("time" = c(0.0, 1.0, 2.0, 3.0, 4.0),
-#'              "time_end" = c(1.0, 2.0, 3.0, 4.0, 5.0),
 #'              "value" = c(1.0, 1.5, 2.0, 1.5, 1.5),
 #'              "upper" = c(3.5, 7.0, 6.5, 5.0, 5.0),
-#'              "lower" = c(0.5, 0.0, 0.5, 0.5, 0.8),
-#'              "item" = "population size")
+#'              "lower" = c(0.5, 0.1, 0.5, 0.5, 0.8))
 #'              
 #' plotPopSizes(df)
 #'
@@ -39,7 +34,7 @@
 #' @importFrom dplyr bind_rows
 #' @importFrom utils head tail
 #' 
-plotPopSizes <- function(df, plot_var = "size", add = FALSE, existing_plot = NULL, col = "#00883a"){
+plotPopSizes <- function(df, add = FALSE, existing_plot = NULL, col = "#00883a"){
   if (add == TRUE && is.null(existing_plot)){
     stop("Please provide an existing plot if you want to add this one.")
   }
