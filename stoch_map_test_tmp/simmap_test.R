d07a94f07213eebf5a4c38b6000ee8af70f6ac90
cmt2 <cmt2@berkeley.edu>
2022-10-27 21:50:18 -1000

    update testing of stoch maps

--- stoch_map_test_tmp/simmap_test.R
+++ stoch_map_test_tmp/simmap_test.R
@@ -5,19 +5,19 @@ tree <- readTrees("stoch_map_test_tmp/tree.nexus")[[1]][[1]]
 
 # read the samples as simmaps
 samples <- readTrace("stoch_map_test_tmp/maps.log")[[1]]$simmap
-simmaps <- lapply(samples, function(x) read.simmap(text = x))
+simmaps <- lapply(samples, function(x) phytools::read.simmap(text = x))
 class(simmaps) <- "multiPhylo"
 
-stoch_map_df <- RevGadgets:::processStochMaps(tree, simmap = simmaps,
+stoch_map_df <- RevGadgets:::processStochMaps(tree, 
+                                              simmap = simmaps,
                                               states = as.character(0:4))
 
 # make colors
 colors <- colFun(5)
-names(colors) <- states
+names(colors) <- as.character(0:4)
 
 RevGadgets:::plotStochMaps(tree = tree,
                            maps = stoch_map_df,
                            color_by = "MAP",
                            colors = colors,
-                           tree_layout = "circular",
                            tip_labels = F)
--- stoch_map_test_tmp/stoch_map_test.R
+++ stoch_map_test_tmp/stoch_map_test.R
@@ -6,13 +6,13 @@ tree <- readTrees("stoch_map_test_tmp/tree.nexus")[[1]][[1]]
 
 # process samples
 stoch_map_df <- RevGadgets:::processStochMaps(tree, "stoch_map_test_tmp/maps.log", 
-                                              states = as.character(1:4), 
+                                              states = as.character(0:4), 
                                               burnin = 0.1)
 
 RevGadgets:::plotStochMaps(tree = tree,
                            maps = stoch_map_df,
                            color_by = "MAP",
                            colors = "default",
-                           tree_layout = "circular",
+                           tree_layout = "rectangular",
                            tip_labels = F)
 
