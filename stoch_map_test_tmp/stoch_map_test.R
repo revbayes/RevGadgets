e9762004fc0a60e610500ea60f9d4522a32306fc
cmt2 <cmt2@berkeley.edu>
2022-10-27 15:02:04 -1000

    updating stoch map code

--- stoch_map_test_tmp/stoch_map_test.R
+++ stoch_map_test_tmp/stoch_map_test.R
@@ -3,13 +3,16 @@
 # read a tree
 tree <- readTrees("stoch_map_test_tmp/tree.nexus")[[1]][[1]]
 
-# process samples
-stoch_map_df <- RevGadgets:::processStochMaps(tree, "stoch_map_test_tmp/maps.log", states = c("0","1"), burnin = 0.1)
 
-#plot samples:
+# process samples
+stoch_map_df <- RevGadgets:::processStochMaps(tree, "stoch_map_test_tmp/maps.log", 
+                                              states = as.character(1:4), 
+                                              burnin = 0.1)
 
 RevGadgets:::plotStochMaps(tree = tree,
                            maps = stoch_map_df,
-                           colors = c("0" = colFun(2)[1], "1" = colFun(2)[2]))
-
+                           color_by = "MAP",
+                           colors = "default",
+                           tree_layout = "circular",
+                           tip_labels = F)
 
